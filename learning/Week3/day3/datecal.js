a68fa575a1647132bbfedf87a731781537c704a0
ranajitbanerjee <ranajit.banerjee@fusioncharts.com>
2015-07-23 11:13:12 +0530

    Update datecal.js

--- learning/Week3/day3/datecal.js
+++ learning/Week3/day3/datecal.js
@@ -10,8 +10,9 @@ function caldifferencedate()
 			var hours=ms/3600000;
 			var days=hours/24;
 			var datediff=document.getElementById('datedifference');
-            
-				datediff.innerHTML=days+" days ";
+			var years=Math.ceil(days/365);
+			var months=(days%365)/30;
+			datediff.innerHTML=days+" days ";
 			
 			
 			
@@ -30,11 +31,15 @@ function caldifferencedate()
 			var time1 = e1.options[e1.selectedIndex].value;
 			var e2 = document.getElementById('amorpm2');
 			var time2 = e2.options[e2.selectedIndex].value;		
-				if(time1=='PM')
+				if(time1=='PM'&&hour1!=12)
 					hour1=hour1+12;
-				if(time2=='PM')
+				if(time2=='PM'&&hour2!=12)
 					hour2=hour2+12;
-					
+				if(time1=='AM'&&hour1==12)
+					hour1=0;				
+				
+				if(time2=='AM'&&hour2==12)
+				   hour2=0;
 			 date1 = new Date(2000, 0, 1,  hour1, minute1); // 9:00 AM
 			if((time2=='PM'&&time2=='AM')||(hour2<hour1))
 			  date2 = new Date(2000, 0, 2,  hour2, minute2);
@@ -60,19 +65,22 @@ function caldifferencedate()
 				 
 				 var hour=parseInt(document.getElementById('hour').value);
 				 var minute=parseInt(document.getElementById('minute').value);
+				 var intervalyear=document.getElementById('timeintervalyrs').value;
+				 var intervalmonth=document.getElementById('timeintervalmonths').value;
 				 var intervalhour=document.getElementById('timeintervalhour').value;
 				 var intervalminute=document.getElementById('timeintervalminute').value;
-					
+				 var intervalday=document.getElementById('timeintervaldays').value;
 	
-				 var timeresult=document.getElementById('timeresult');
-				 var timeresult2=document.getElementById('timeresult2');
-				 
-				 var date1=new Date(dat.getFullYear(),dat.getMonth(),dat.getDate(),hour,minute);
+				 var timeresult=document.getElementById('timeresult2');
 				
+				 var date1=new Date(dat.getFullYear(),dat.getMonth(),dat.getDate(),hour,minute);
+				 date1.setFullYear(date1.getFullYear()+parseInt(intervalyear));
+				 date1.setMonth(date1.getMonth()+parseInt(intervalmonth));
 				 date1.setHours(date1.getHours()+parseInt(intervalhour));
 				 date1.setMinutes(date1.getMinutes()+parseInt(intervalminute));
+				 date1.setDate(date1.getDate()+parseInt(intervalday));
 				 var datestring=date1.toString();
-				 timeresult2.innerHTML=datestring.substring(0,datestring.indexOf('G'));
+				 timeresult.innerHTML=datestring.substring(0,datestring.indexOf('G'));
 			   // timeresult2.innerHTML=date1.getDate()+"/"+date1.getMonth()+"/"+date1.getFullYear()+"  "+date1.getHours()+":"+date1.getMinutes();
 			  	// alert(date1.getMinutes()); 			
 				 				
