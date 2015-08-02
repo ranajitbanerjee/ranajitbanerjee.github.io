52c1857bddd862bd41c7a3b93af78de21b7b34c0
unknown <ranajit.banerjee@fusioncharts.com>
2015-08-03 00:44:39 +0530

    added week4 project

---
+++ learning/Week4/Day5/formdata.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 02, 2015 at 07:13 PM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.4.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `subscription`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formdata`
+--
+
+CREATE TABLE IF NOT EXISTS `formdata` (
+  `name` varchar(200) NOT NULL,
+  `sex` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `country` varchar(200) NOT NULL,
+  `state` varchar(200) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `mobileno` varchar(200) NOT NULL,
+  `interest` varchar(200) NOT NULL,
+  `favorites` varchar(200) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
