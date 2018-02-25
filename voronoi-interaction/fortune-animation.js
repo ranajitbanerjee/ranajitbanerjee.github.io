7f46fc802be037392b4863651d6278fcdc87760c
Ranajit Banerjee <ranajit.banerjee@fusioncharts.com>
2018-02-25 23:51:49 +0530

    Correct sample

--- voronoi-interaction/fortune-animation.js
+++ voronoi-interaction/fortune-animation.js
@@ -1,7 +1,5 @@
 Voronoi.canvas = document.getElementById('myCanvas');
 Voronoi.init();
-Voronoi.canvas.width = width;
-Voronoi.canvas.height = height;
 // Get the data
 d3.json("./cars.json", function(error, data) {
     if (error) throw error;
@@ -9,6 +7,8 @@ d3.json("./cars.json", function(error, data) {
     let plot = new ScatterPlot().data(data).width(870).height(500);
 
     let points = plot.getDataPoints();
+    Voronoi.canvas.width = plot._width;
+    Voronoi.canvas.height = plot._height;
     Voronoi.sites = points.map(d => new Voronoi.Site(d.x, d.y));
     VoronoiAnimate(10, 100);
   });
